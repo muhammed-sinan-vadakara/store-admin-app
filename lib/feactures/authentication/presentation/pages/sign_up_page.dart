d06e84cfefdb4a735127b32114573937e7bf76e2
muhammed-sinan=vadakara <muhammedsinanvadakara@gmail.com>
2024-04-01 16:18:34 +0530

    firestore added

--- lib/feactures/authentication/presentation/pages/sign_up_page.dart
+++ lib/feactures/authentication/presentation/pages/sign_up_page.dart
@@ -1,14 +1,3 @@
-// import 'package:flutter/widgets.dart';
-
-// class SignUpPage extends StatelessWidget {
-//   const SignUpPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
